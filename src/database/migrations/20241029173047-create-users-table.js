5d57a1e8595cc768b0071fae34aa1627b1f02337
jonadableite <jonadab.leite@gmail.com>
2024-10-30 16:51:05 -0300

    Configuração do Model de Usuário

--- src/database/migrations/20241029173047-create-users-table.js
+++ src/database/migrations/20241029173047-create-users-table.js
@@ -22,7 +22,7 @@ module.exports = {
         unique: true,
       },
 
-      password: {
+      password_hash: {
         type: Sequelize.STRING,
         allowNull: false,
       },
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const router = new Router();
 
-router.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Hello World!' })
-})
+router.get('/', async (req, res) => {
+  const users = await User.create({
+    id: v4(),
+    name: 'Jonadab Leite',
+    email: 'jonadab.leite@gmail.com',
+    password_hash: '123456',
+  });
 
-export default router
+  return res.status(201).json(users);
+});
+
+export default router;
